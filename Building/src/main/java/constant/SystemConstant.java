21f566ff6385b75e747baa2fa86a3966efeed93a
trangntb <nguyenthibaotrang@gmail.com>
2023-03-26 23:05:02 +0700

    ok

---
+++ Building/src/main/java/constant/SystemConstant.java
@@ -0,0 +1,14 @@
+package constant;
+
+public class SystemConstant {
+	public static final String ONE_EQUAL_ONE ="where 1=1 ";
+//	public static final String[] TYPE_CODE = {"tang-tret", "noi-that", "nguyen-can"};
+//	public static final String[] TYPE_NAME = {"Tầng trệt","Nguyên căn","Nội thất"};
+	
+	public static final String TANG_TRET_CODE = "tang-tret";
+	public static final String NGUYEN_CAN_CODE = "nguyen-can";
+	public static final String NOI_THAT_CODE = "noi-that";
+	public static final String TANG_TRET_VALUE = "Tầng trệt";
+	public static final String NGUYEN_CAN_VALUE = "Nguyên căn";
+	public static final String NOI_THAT_VALUE = "Nội thất";
+}
